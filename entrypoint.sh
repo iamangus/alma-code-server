771fe6110a2b7a952c39fa9e0d04f6e16c30a8c1
iamangus <turnbull.angus@gmail.com>
2024-05-30 16:18:21 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,13 @@ code-server --install-extension golang.go &
 
 code-server --install-extension ms-python.python &
 
-sudo useradd -m ${USER_NAME}
+sudo useradd -s $(which zsh) -m ${USER_NAME}
 
 sudo chown -R ${USER_NAME}:${USER_NAME} /home/${USER_NAME} 
 
 sudo echo "${USER_NAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER_NAME}
 
 sudo chsh -s $(which zsh)
-
-su $USER_NAME --command "chsh -s $(which zsh)"
 
 su $USER_NAME --command "code-server --auth none --bind-addr 0.0.0.0:8080 '/home/${USER_NAME}'"
 
