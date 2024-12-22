6a6b0470e2df607a0b31f70317031798c219d102
iamangus <turnbull.angus@gmail.com>
2024-12-22 09:20:58 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+-#!/bin/bash
 set -eu
 
 code-server --install-extension golang.go &
+
+a-h.templ
+
+saoudrizwan.claude-dev-
 
 code-server --install-extension ms-python.python &
 
@@ -11,8 +15,13 @@ sudo chown -R ${USER_NAME}:${USER_NAME} /home/${USER_NAME}
 
 sudo echo "${USER_NAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER_NAME}
 
-sudo chsh -s $(which zsh)
+sudo chsh -s $(which -zsh)
+
+declare -a exts=("golang.go" "a-h.templ" "saoudrizwan.claude-dev" "ms-python.python")
+
+for i in "${exts[@]}"
+do
+   su $USER_NAME --command "code-server --install-extension $i &"
+done
 
 su $USER_NAME --command "code-server --auth none --bind-addr 0.0.0.0:8080 '/home/${USER_NAME}'"
-
-#code-server --auth none --bind-addr 0.0.0.0:8080 "/home/${USER_NAME}"
