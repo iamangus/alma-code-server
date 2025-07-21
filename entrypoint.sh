d5743ad305168a9ad3025426ccc2189c387ca80f
iamangus <turnbull.angus@gmail.com>
2025-07-21 07:56:40 -0500

    adding ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
 
-code-server --install-extension golang.go &
+#code-server --install-extension golang.go &
 
-code-server --install-extension ms-python.python &
+#code-server --install-extension ms-python.python &
 
 sudo useradd -s $(which zsh) -m ${USER_NAME}
 
@@ -13,11 +13,15 @@ sudo echo "${USER_NAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER_NAME}
 
 sudo chsh -s $(which zsh)
 
-#declare -a exts=("golang.go" "a-h.templ" "saoudrizwan.claude-dev" "ms-python.python")
+su $USER_NAME --command "curl https://github.com/iamangus.keys | tee -a /home/angoo/.ssh/authorized_keys"
 
-#for i in "${exts[@]}"
-#do
-#   su $USER_NAME --command "code-server --install-extension $i &"
-#done
+declare -a exts=("golang.go" "a-h.templ" "saoudrizwan.claude-dev" "ms-python.python")
+
+for i in "${exts[@]}"
+do
+   su $USER_NAME --command "code-server --install-extension $i &"
+done
+
+/usr/sbin/sshd -D &
 
 su $USER_NAME --command "code-server --auth none --bind-addr 0.0.0.0:8080 '/home/${USER_NAME}'"
